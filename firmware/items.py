ab56c5418145207d79b4777dbdb4abb31ec5aba0
kydahe <37690316+kydahe@users.noreply.github.com>
2019-07-17 14:02:51 +0800

    Update items.py

--- firmware/items.py
+++ firmware/items.py
@@ -1,13 +1,13 @@
 from scrapy.item import Item, Field
-
+#保存爬取数据的容器
 class FirmwareImage(Item):
     product = Field(default=None)
-    vendor = Field()
+    vendor = Field()#制造商
 
-    description = Field(default=None)
-    version = Field(default=None)
-    build = Field(default=None)
-    date = Field(default=None)
+    description = Field(default=None)#描述
+    version = Field(default=None)#版本
+    build = Field(default=None)#
+    date = Field(default=None)#日期
 
     mib = Field(default=None)
     sdk = Field(default=None)
